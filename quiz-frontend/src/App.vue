bce284406d65eee32ed235de4d3255e49e1b9fe3
Ku1an <simondanielssoncsgo@gmail.com>
2025-09-20 23:37:46 +0200

    Start of QuizWalk project

---
+++ quiz-frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-img
+      class="position-fixed ma-4 mt-6"
+      :width="100"
+      height="60"
+      aspect-ratio="16/9"
+      src="/quiz.png"
+      style="opacity: 0.7;"
+    ></v-img>
+    <v-main>
+      <Landing />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import Landing from "./views/Landing.vue";
+
+//
+</script>
+
+<style>
+</style>
---
+++ quiz-frontend/src/views/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex flex-column align-center mt-16 fill-height">
+            <h1 class="mt-16">Quiz Walk</h1>
+            <v-divider :thickness="3" class="border-opacity-25 w-50 mt-8" ></v-divider>
+            <v-row class="mt-10">
+                <v-col cols="6" class="">
+                    <v-btn color="primary">Play Quiz</v-btn>
+                </v-col>
+                <v-col cols="6">
+                    <v-btn color="warning">Admin Page</v-btn>
+                </v-col>
+            </v-row>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
